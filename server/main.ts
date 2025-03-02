968e33a23bda7653f50028fae6dafdd6b2636cd6
Tekalig <tekahazi06@gmail.com>
2025-03-02 23:07:48 +0300

    feat: integrate API routes and enhance CORS support in main application

--- server/main.ts
+++ server/main.ts
@@ -1,36 +1,39 @@
 import { Application } from "jsr:@oak/oak/application";
+import api from "./src/routes/api.ts";
 
 const app = new Application();
 const PORT = 8000;
 
-app.use(async(ctx, next) => {
+app.use(async (ctx, next) => {
+  ctx.response.headers.set(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173",
+  );
   await next();
   const time = ctx.response.headers.get("X-Response-Time");
-  console.log(`HTTP ${ctx.request.method} on ${ctx.request.url} at ${new Date().toISOString()} [${time}]`);
+  console.log(
+    `HTTP ${ctx.request.method} on ${ctx.request.url} at ${
+      new Date().toISOString()
+    } [${time}]`,
+  );
 });
 
-app.use(async(ctx, next)=> {
+app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
-  const delta = Date.now()- start;
+  const delta = Date.now() - start;
   ctx.response.headers.set("X-Response-Time", `${delta}ms`);
-})
-
-app.use(async(ctx) => {
-  ctx.response.body = "Hello, World!";
 });
 
-app.use((ctx) => {
-  ctx.response.body = `
-  +----------------------------------+
-  |          Mission Control         |
-  +----------------------------------+
-  | Status: All systems operational  |
-  | Next Check: 2023-10-01 12:00 UTC |
-  +----------------------------------+
-  `;
-});
+app.use(api.routes());
+app.use(api.allowedMethods());
 
+app.addEventListener("listen", ({ port, serverType }) => {
+  console.log(
+    `Start listening on http://localhost:${port}`,
+  );
+  console.log(`using HTTP server: ${serverType}`);
+});
 
 if (import.meta.main) {
   await app.listen({ port: PORT });
